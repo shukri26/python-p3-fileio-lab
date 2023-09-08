18aff321a9971a950d8e10666580df1ed3dcfb83
Tim <mohamud.hussein@student.moringaschool.com>
2023-09-08 23:51:34 +0300

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
+# lib/file_io.py
+
 def write_file(file_name, file_content):
-    pass
+    with open(file_name, "w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(file_name, "a") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(file_name, "r") as file:
+        content = file.read()
+        return content
